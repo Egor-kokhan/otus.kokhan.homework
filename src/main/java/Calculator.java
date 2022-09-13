7ec2bc8898827862291d362bc8b6ba5d4da1173d
e.kohan <e.kohan@smartdeal.pro>
2022-09-13 18:54:43 +0300

    add solution of a quadratic equation with tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,8 +1,34 @@
+import java.util.Arrays;
+
 public class Calculator {
 
+    private final static double eps = 0.00001;
 
-    public int sum(int a, int b) {
-        return a+b;
+    public double[] solve(double a, double b, double c) {
+        if (Double.isInfinite(a) || Double.isInfinite(b) || Double.isInfinite(c)) {
+            return new double[]{};
+        }
+        if (Math.abs(a) < eps) {
+            throw new RuntimeException("No decision");
+        }
+        double d = Math.pow(b, 2) - 4 * a * c;
+        if (Double.isInfinite(d)) {
+            return new double[]{};
+        }
+        if (d < 0) {
+            return new double[]{};
+        }
+        if (d == 0) {
+            return new double[]{-b / (2 * a)};
+        }
+
+        double[] rez = new double[2];
+
+        rez[0] = (-b + Math.sqrt(d)) / (2 * a);
+        rez[1] = (-b - Math.sqrt(d)) / (2 * a);
+        Arrays.sort(rez);
+
+        return rez;
     }
 
 }
